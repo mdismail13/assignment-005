88b7eb39f8560d1d062349f00f0dfb627b3cea3f
mdismail13 <mdismail.connect@gmail.com>
2025-08-28 04:34:18 +0600

    made responsive for small devices

--- js/script.js
+++ js/script.js
@@ -51,3 +51,47 @@ document
         document.getElementById('copy-counter').innerText = copyCounter + 1;    
     }
   });
+
+  const copyBtn = document.getElementsByClassName('fa-copy');
+
+  for(const btn of copyBtn){
+    btn.addEventListener('click', function(event){
+        const text = event.target.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].innerText;
+        navigator.clipboard.writeText(text);
+        alert('Number copied!');
+        const copyCounter = Number(document.getElementById('copy-counter').innerText);
+        document.getElementById('copy-counter').innerText = copyCounter + 1;
+    })
+  }
+
+  const callBtn = document.getElementsByClassName('fa-phone');
+
+  for(const btn of callBtn){
+    btn.addEventListener('click', function(event){
+        const serviceName = event.target.parentNode.parentNode.parentNode.childNodes[3].innerText;
+        const serviceNumber = event.target.parentNode.parentNode.parentNode.childNodes[7].childNodes[1].innerText;
+        const coin = Number(document.getElementById('coin').innerText);
+        if(coin < 20){
+            alert("❌ You don't have sufficient coins. Minimum 20 coins required to call😥")
+            return;
+        }
+        alert("📞 Calling " + serviceName + " " + serviceNumber +"...");
+        document.getElementById('coin').innerText = coin - 20;
+
+        const date = new Date().toLocaleTimeString();
+        const history = document.createElement('div');
+        
+        history.innerHTML = `
+            <div>
+                <p class = "text-[18px] font-semibold text-[#111111]">${serviceName}</p>
+                <p class = "text-[18px] text-[#5C5C5C]">${serviceNumber}</p>
+            </div>
+            <div>
+            <p>${date}</p>
+            </div>
+        `;
+        history.classList.add('flex', 'justify-between', 'items-center', 'p-4', 'mb-2', 'bg-[#FAFAFA]', 'rounded-xl');
+
+        document.getElementById('call-history').append(history);
+    })
+  }
